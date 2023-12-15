d681a99a409876b8616c6129a9a09a774bc19ab5
Abhishek7Tech <abhishektab45@gmail.com>
2023-12-15 14:10:39 +0530

    implemented navbar

---
+++ src/components/main/main.js
@@ -0,0 +1,13 @@
+import Navbar from "../navbar/navbar";
+import "./main.css";
+const Main =  () => {
+return (
+    <>
+    <div className="main">
+        <Navbar></Navbar>
+    </div>
+    </>
+)
+}
+
+export default Main;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="flex flex-row justify-around pt-6 pb-2 items-center">
+            <h3 className=" text-gray-700 font-bold text-2xl">UniCraft</h3>
+            <ul className="basis-1/4 flex flex-row justify-between">
+                <li className="text-gray-700 text-lg font-medium">How it works</li>
+                <li className="text-gray-700 text-lg font-medium">Pricing</li>
+                <li className="text-gray-700 text-lg font-medium">FAQ</li>
+            </ul>
+            <button className="text-gray-600 text-lg font-bold border-gray-600 rounded-lg border-2 w-36 h-12">Contact us</button>
+        </nav>
+    )
+}
+
+export default Navbar;
